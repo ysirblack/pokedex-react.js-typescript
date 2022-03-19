783fa6ffca0707927ed1b04cc30565b4b4c07533
Yasir Kara <yasir.kara@hotmail.com>
2022-03-19 17:23:43 +0300

    loading gif

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Spinner from "../components/layout/Spinner";
+
 function Home() {
   const [count, setCount] = useState(0);
   const [loading, setLoading] = useState(true);
   const [pokemons, setPokemons] = useState([{}]);
 
-  useEffect(()=> {
+  useEffect(() => {
     fetchPokemons();
-  },[])
+  }, []);
 
-  const fetchPokemons = async () =>  {
+  const fetchPokemons = async () => {
     const response = await fetch(`${process.env.REACT_APP_POKEMON_API_URL}/pokemon/?limit=1126`);
 
     const data = await response.json();
@@ -18,25 +20,27 @@ function Home() {
     setPokemons(data.results);
     setLoading(false);
     console.log(pokemons[0].name);
+  };
 
-
-  }
-  if(!loading){
+  if (!loading) {
     return (
       <div>
-        <h1 className="text-3xl mb-3"> Search for a Pokemon among number of <span className="text-red-500">{count}!</span></h1>
-        <div className='grid grid-cols-2 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+        <h1 className="text-3xl mb-3">
+          {" "}
+          Search for a Pokemon among number of <span className="text-red-500">{count}!</span>
+        </h1>
+        <div className="grid grid-cols-2 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
           {pokemons.map((pokemon) => (
-            <p><Link to="/pokemon">{pokemon.name}</Link></p>
+            <p>
+              <Link to="/pokemon">{pokemon.name}</Link>
+            </p>
           ))}
         </div>
       </div>
     );
+  } else {
+    return <Spinner />
   }
-  else{
-   return <h3> Loading..</h3>
-  }
- 
 }
 
 export default Home;
