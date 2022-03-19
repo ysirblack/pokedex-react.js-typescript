01991645ed29b19ff0065da56093be2be14e7d29
Yasir Kara <yasir.kara@hotmail.com>
2022-03-19 13:51:51 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
-        <main>content</main>
+        <main className="container mx-auto px-3 pokedex">
+          <img src="pokedex.png" alt="pokedex"></img>
+        </main>
         <Routes></Routes>
+        <Footer />
       </div>
     </Router>
   );
---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { SiPokemon } from "react-icons/si";
+const footerYear = new Date().getFullYear();
+
+function Footer() {
+  return (
+    <footer className="footer pb-3 bg-red-600 text-primary-content footer-center">
+      <div>
+        <SiPokemon className="text-8xl" />
+        <p>Made with ❤️ by y`Sir bLack</p>
+        <p>Copyright &copy; {footerYear} All rights reserved</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
